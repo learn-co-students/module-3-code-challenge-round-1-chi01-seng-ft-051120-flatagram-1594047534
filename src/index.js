b68190abf255501b6460140de1b77aff723f5686
skandow <kando1sr@gmail.com>
2020-07-06 11:01:50 -0500

    basic deliverables met

--- src/index.js
+++ src/index.js
@@ -1,1 +1,100 @@
 // write your code here
+document.addEventListener("DOMContentLoaded", renderPage)
+
+function renderPage() {
+    fetchImage();
+    
+}
+
+function fetchImage() {
+    fetch("http://localhost:3000/images/1")
+    .then(resp => resp.json())
+    .then(json => renderJSON(json))
+}
+
+function renderJSON(json) {
+    const title = document.getElementsByClassName("title")[`${json.id - 1}`]
+    title.innerHTML = json.title 
+    title.nextElementSibling.src = `${json.image}`
+    var likesMessage 
+    if (json.likes === 1) {
+        likesMessage = "like"
+    } else {
+        likesMessage = "likes"
+    }
+    title.nextElementSibling.nextElementSibling.innerHTML = `<span id="likes-${json.id}"
+    class="likes">${json.likes} ${likesMessage}</span>
+    <button data-photo-id="${json.id}" data-likes-count="${json.likes}" class="like-button">♥</button>`
+    renderComments(json);
+    renderLikeEvent();
+    renderCommentEvent()
+}
+
+function renderComments(json) {
+    const commentList = document.getElementsByClassName("comments")[`${json.id - 1}`]
+    commentList.innerHTML = ``
+    json.comments.forEach(comment => {
+        renderComment(commentList, comment)
+    })
+}
+
+function renderComment(commentList, comment) {
+    commentList.innerHTML += `<li id="comment-${comment.id}">${comment.content}</li>`
+}
+
+function handleEvents(event) {
+    if (event.target.className = "like-button") {
+        addLike(event)
+    }
+}
+
+function addLike(event) {
+    const url = "http://localhost:3000/images/1"
+    const likesNumber = parseInt(event.target.dataset.likesCount) 
+    const payload = {likes: likesNumber + 1}
+    const reqObj = {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    }
+    fetch(url, reqObj)
+    .then(resp => resp.json())
+    .then(json => fetchImage())
+}
+
+function renderLikeEvent() {
+    const likeButtons = document.getElementsByClassName("like-button")
+    for (let i=0; i<likeButtons.length; i++) {
+        likeButtons[i].addEventListener("click", addLike)
+    }
+}
+
+function renderCommentEvent() {
+    const commentsForms = document.getElementsByClassName("comment-form")
+    for (let i=0; i<commentsForms.length; i++) {
+        commentsForms[i].addEventListener("submit", addComment)
+    }
+}
+
+function addComment() {
+    event.preventDefault();
+    const comment = event.target.comment.value 
+    const payload = {
+        imageId: 1,
+        content: comment
+    }
+    event.target.reset();
+    
+    const reqObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    }
+    fetch("http://localhost:3000/comments", reqObj)
+    .then(resp => resp.json())
+    .then(json => fetchImage())
+    }
